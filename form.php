4201d285a851154a34c466b56061f4382769c1c6
alejocardoso <alejocardoso@Alejos-MacBook-Air.local>
2022-04-27 02:07:34 -0300

    actualizacion

--- form.php
+++ form.php
@@ -9,14 +9,13 @@ $mensaje = $_POST['textarea'];
  // como llegará el email:
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . ",\r\n";
-$mensaje .= "El asunto es: " $asunto . ",\r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . ",\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time())
 
 $para = 'aacar.ph@gmail.com'
 $asunto = 'Este mail fue enviado desde la web'
 
-// funcion mail, a quien le mando el mail
+// funcion mail, a quien le llega el mail
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
 // redirección al haber enviado el form
